4aaef5a3406793dfba8537c453622a3ea7898f54
Pathompong <Pathompong.forwork@gmail.com>
2025-01-09 16:58:29 +0700

    [#Go] prepare api for login and resgister module

--- backend/cmd/main.go
+++ backend/cmd/main.go
@@ -14,46 +14,34 @@ import (
 )
 
 func main() {
-	if err := godotenv.Load("./.env"); err != nil {
-		log.Println("No .env file found")
+	if err := godotenv.Load("../.env"); err != nil {
+		log.Fatal("Error loading .env file")
 	}
 
-
-
-	uri := os.Getenv("MONGODB_URI")
-	fmt.Println(uri)
+	MONGODB_URI := os.Getenv("MONGODB_URI")
 	docs := "www.mongodb.com/docs/drivers/go/current/"
-	if uri == "" {
+	if MONGODB_URI == "" {
 		log.Fatal("Set your 'MONGODB_URI' environment variable. " +
 			"See: " + docs +
 			"usage-examples/#environment-variable")
 	}
-
-
-	client, err := mongo.Connect(context.TODO(), options.Client().
-		ApplyURI(uri))
+	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(MONGODB_URI)	)
 	if err != nil {
 		panic(err)
 	}
-	fmt.Println("Connected !!")
 
-
-	defer func() {
-		if err := client.Disconnect(context.TODO()); err != nil {
+	defer func(){
+		if err = client.Disconnect(context.TODO()); err != nil {
 			panic(err)
 		}
 	}()
 
-	coll := client.Database("sample_mflix").Collection("movies")
-	title := "Back to the Future"
+	coll := client.Database("my_database").Collection("my_collection")
+	name := "John Doe"
 
 	var result bson.M
-	err = coll.FindOne(context.TODO(), bson.D{{"title", title}}).
-		Decode(&result)
-	if err == mongo.ErrNoDocuments {
-		fmt.Printf("No document was found with the title %s\n", title)
-		return
-	}
+	err = coll.FindOne(context.TODO(), bson.M{"name": name}).Decode(&result)
+
 	if err != nil {
 		panic(err)
 	}

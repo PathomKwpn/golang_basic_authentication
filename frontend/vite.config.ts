0b5ee4cd89f4f4ab4a6a26397d1d995d1479006d
Pathompong <Pathompong.forwork@gmail.com>
2025-07-25 16:28:16 +0700

    [#CONFIG] Add alias path '@' assign './src'

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,5 +1,12 @@
 import { defineConfig } from "vite";
+import path from "path";
 import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
   plugins: [tailwindcss()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+      "@components": path.resolve(__dirname, "./src/modules/components"),
+    },
+  },
 });
